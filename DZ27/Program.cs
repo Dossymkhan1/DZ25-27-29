0e5f901e108fbeb62d1187e88af0b26520fb5025
Your_name <dosss_xa@example.com>
2022-12-18 13:19:41 +0600

    added new dz25

---
+++ DZ27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает
+//  на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int CountNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int x =num%10;
+        sum += x;
+        num /= 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр в числе {n} = {CountNum(n)}");
